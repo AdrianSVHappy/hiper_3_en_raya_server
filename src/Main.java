4d9a4e2f01e1bee33778867b4f9a6d84a2582c02
AdrianSVHappy <adriansuarezvaldayo573@gmail.com>
2024-12-26 22:33:40 +0100

    En blanco

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * <h1>Name.java</h1><hr>
+ * <p>Esta clase represanta </p>
+ *
+ * @author Adrián Suárez Valdayo
+ * @vesrion 26-dic-2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server");
+    }
+}
